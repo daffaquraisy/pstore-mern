6d3868634d9c1147eebd9116afd25a12764dd77f
daffaquraisy <daffaquraisy@gmail.com>
2020-12-03 12:30:05 +0700

    setup server done

---
+++ server/config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://daffa:<nulgath1>@cluster0.s7uch.mongodb.net/<dbname>?retryWrites=true&w=majority",
+};
---
+++ server/routes/users.js
@@ -0,0 +1,271 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models/User");
+const { Product } = require("../models/Product");
+const { Payment } = require("../models/Payment");
+const { auth } = require("../middleware/auth");
+const async = require("async");
+
+//=================================
+//             User
+//=================================
+
+router.get("/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+    cart: req.user.cart,
+    history: req.user.history,
+  });
+});
+
+router.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
+
+router.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found",
+      });
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "Wrong password" });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie("w_authExp", user.tokenExp);
+        res.cookie("w_auth", user.token).status(200).json({
+          loginSuccess: true,
+          userId: user._id,
+        });
+      });
+    });
+  });
+});
+
+router.get("/logout", auth, (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.user._id },
+    { token: "", tokenExp: "" },
+    (err, doc) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true,
+      });
+    }
+  );
+});
+
+router.post("/addToCart", auth, (req, res) => {
+  const { _id } = req.user;
+  const { productId } = req.query;
+
+  User.findOne({ _id }, (err, userInfo) => {
+    let duplicate = false;
+
+    userInfo.cart.forEach((cartInfo) => {
+      if (cartInfo.id === productId) {
+        // this product already exists in the cart
+        duplicate = true;
+      }
+    });
+
+    // if theres a duplicate product in cart, we will duplicate the qty
+    if (duplicate) {
+      User.findOneAndUpdate(
+        {
+          _id: req.user._id,
+          "cart.id": req.query.productId,
+        },
+        {
+          $inc: { "cart.$.quantity": 1 },
+        },
+        {
+          // then always update the data if the qty is change
+          new: true,
+        },
+        (err) => {
+          if (err) return res.json({ success: false, err });
+          res.status(200).json(userInfo.cart);
+        }
+      );
+    } else {
+      User.findOneAndUpdate(
+        {
+          _id: req.user._id,
+        },
+        {
+          $push: {
+            cart: {
+              id: req.query.productId,
+              quantity: 1,
+              date: Date.now(),
+            },
+          },
+        },
+        {
+          new: true,
+        },
+        (err, userInfo) => {
+          if (err) return res.json({ success: false, err });
+          res.status(200).json(userInfo.cart);
+        }
+      );
+    }
+  });
+});
+
+router.get("/removeFromCart", auth, (req, res) => {
+  const { _id } = req.user;
+  const { _id: productId } = req.query;
+
+  User.findOneAndUpdate(
+    { _id },
+    {
+      $pull: {
+        cart: {
+          id: productId,
+        },
+      },
+    },
+    { new: true },
+    (err, userInfo) => {
+      let { cart } = userInfo;
+      let cartItemsIdArray = [];
+      cart.forEach(
+        (item) => (cartItemsIdArray = [...cartItemsIdArray, item.id])
+      );
+
+      // go to Product model
+      Product.find({ _id: { $in: cartItemsIdArray } })
+        .populate("writer")
+        .exec((err, products) => {
+          return res.status(200).json({
+            cart, // in reducer
+            cartDetail: products, // in reducer
+          });
+        });
+    }
+  );
+});
+
+router.get("/userCartInfo", auth, (req, res) => {
+  User.findOne({ _id: req.user._id }, (err, userInfo) => {
+    let cart = userInfo.cart;
+    let array = cart.map((item) => {
+      return item.id;
+    });
+
+    Product.find({ _id: { $in: array } })
+      .populate("write")
+      .exec((err, cartDetail) => {
+        if (err) return res.status(400).send(err);
+        return res.status(200).json({ success: true, cartDetail, cart });
+      });
+  });
+});
+
+router.post("/successBuy", auth, (req, res) => {
+  let history = [];
+  let transactionData = {};
+
+  // 1. put brief payment inside user collect
+  req.body.cartDetail.forEach((item) => {
+    history.push({
+      dateOfPurchase: Date.now(),
+      name: item.title,
+      id: item._id,
+      price: item.price,
+      quantity: item.quantity,
+      paymentId: req.body.paymentData.paymentID,
+    });
+  });
+
+  // 2.put payment information that come from paypal collection
+  transactionData.user = {
+    id: req.user._id,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    email: req.user.email,
+  };
+
+  transactionData.data = req.body.paymentData;
+  transactionData.product = history;
+
+  // empty the cart
+  User.findOneAndUpdate(
+    { _id: req.user._id },
+    { $push: { history: history }, $set: { cart: [] } },
+    { new: true },
+    (err, user) => {
+      if (err) return res.json({ success: false, err });
+
+      const payment = new Payment(transactionData);
+      payment.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+
+        // 3. increase the amount of number for the sold information
+
+        // first we need to know how many product were sold in this transaction for each of products
+
+        let products = [];
+        doc.product.forEach((item) => {
+          products.push({ id: item.id, quantity: item.quantity });
+        });
+
+        async.eachSeries(
+          products,
+          (item, callback) => {
+            Product.update(
+              { _id: item.id },
+              {
+                $inc: {
+                  sold: item.quantity,
+                },
+              },
+              { new: false },
+              callback
+            );
+          },
+          (err) => {
+            if (err) return res.json({ success: false, err });
+
+            return res.status(200).json({
+              success: true,
+              cart: user.cart,
+              cartDetail: [],
+            });
+          }
+        );
+      });
+    }
+  );
+});
+
+router.get("/getHistory", auth, (req, res) => {
+  User.findOne({ _id: req.user._id }, (err, doc) => {
+    let history = doc.history;
+    if (err) return res.status(400).send(err);
+
+    return res.status(200).json({ success: true, history });
+  });
+});
+
+module.exports = router;
